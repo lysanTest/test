93de4f72a432089e9d800d7c338260824b4fa804
李姗 <lishan@hekonggroup.com>
2018-04-17 15:34:01 +0800

    '测试'

---
+++ config/prod.env.js
@@ -0,0 +1,11 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  API_HOST:'"http://www.renwubang.cn/"'   
+}
+// http://www.renwubang.cn/
+
+//http://39.104.56.63:8080/
+
+
+// http://192.168.0.200:80/
